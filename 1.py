edff8b9b5493268ef4b58ac6c0a6fe5dee11b400
D875 <53596553+D875@users.noreply.github.com>
2019-08-03 21:31:43 +0800

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Capstone project
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+import numpy as np
+
+
+# In[3]:
+
+
+print ("Hello Capstone Project Course!")
+
